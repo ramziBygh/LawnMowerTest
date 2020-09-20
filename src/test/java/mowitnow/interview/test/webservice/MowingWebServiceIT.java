c120e4fa5ca68f24e6ef6cc6e52517dd144c5b62
ramzi ben yahia <ramzi.benyahya@ensi-uma.tn>
2020-09-20 11:52:47 +0200

    remove unused imports

--- src/test/java/mowitnow/interview/test/webservice/MowingWebServiceIT.java
+++ src/test/java/mowitnow/interview/test/webservice/MowingWebServiceIT.java
@@ -3,38 +3,22 @@ package mowitnow.interview.test.webservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oracle.tools.packager.IOUtils;
 import mowitnow.interview.test.service.MowingService;
-import mowitnow.interview.test.service.MowingServiceImpl;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
 
 @RunWith(SpringRunner.class)
